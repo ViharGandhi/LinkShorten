f72a5f89358fa329ec0eab9d4a7b3cc385760c5f
ViharGandhi <viharworks247@gmail.com>
2023-05-16 11:13:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 function App() {
+  const [url,seturl] = useState('')
+  const[shorturl,setshorturl] = useState('')
+  const handlesubmit = async(e)=>{
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://api.shrtco.de/v2/shorten?url=${url}`
+      )
+      const data = await response.json()
+      setshorturl(data.result.full_short_link);
+    } catch (e) {
+      alert(e);
+    }
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form onSubmit={handlesubmit}>
+      <input value={url} onChange={e => seturl(e.target.value)}/>
+      <button>Short</button>
+     </form>
+    {shorturl&&
+  <div className='shortener__viewShot'>
+  {shorturl}
+  <CopyToClipboard text={shorturl}>
+    <button onClick={() => alert("The URL has been copied")}>copy</button>
+  </CopyToClipboard>
+</div>
+}
     </div>
   );
 }
